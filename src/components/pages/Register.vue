bd1baa22c044278abc882a46596cc87ea0f73cfb
lx812833 <lx812833@163.com>
2018-12-21 21:38:17 +0800

    用户注册

--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -14,6 +14,7 @@
         placeholder="请输入用户名"
         required
         @click-icon="username = ''"
+        :error-message="usernameErrorMsg"
       />
       <van-field
         v-model="password"
@@ -21,27 +22,82 @@
         label="密码"
         placeholder="请输入密码"
         required
+        :error-message="passwordErrorMsg"
       />
       <div class="register-button">
         <van-button
           type="primary"
           size="large"
+          :loading="openLoading"
+          @click="registerAction"
         >马上注册</van-button>
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
+import { Toast } from "vant";
+import url from "@/serverConfig";
 export default {
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      openLoading: false, // 是否开启用户注册的loading状态
+      usernameErrorMsg: "", // 当用户名出现错误的时候
+      passwordErrorMsg: ""
     };
   },
   methods: {
     goBack() {
-      this.$router.go(-1);  // 返回前一页
+      this.$router.go(-1); // 返回前一页
+    },
+    axiosRegisterUser() {
+      this.openLoading = true;
+      axios({
+        url: url.registerUser,
+        method: "post",
+        data: {
+          userName: this.username,
+          passWord: this.password
+        }
+      })
+        .then(res => {
+          console.log(res);
+          if (res.data.code == 200) {
+            Toast.success(res.data.message);
+            this.$router.push("/");
+          } else {
+            this.openLoading = false;
+            Toast.fail("注册失败");
+          }
+        })
+        .catch(error => {
+          Toast.fail("注册失败");
+          this.openLoading = false;
+        });
+    },
+    // 表单验证
+    checkForm() {
+      let isOk = true;
+      if (this.username.length < 1) {
+        this.usernameErrorMsg = "用户名不能少于2位";
+        isOk = false;
+      } else {
+        this.usernameErrorMsg = "";
+      }
+      if (this.password.length < 6) {
+        this.passwordErrorMsg = "密码不能少于6位";
+        isOk = false;
+      } else {
+        this.passwordErrorMsg = "";
+      }
+      return isOk;
+    },
+    registerAction() {
+      this.checkForm() && this.axiosRegisterUser();
+      // checkForm方法为true时才执行axiosRegisterUser()方法
     }
   }
 };
