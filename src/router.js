ab60933ece6766ce463c3afe8afcb1cb6a123e6b
lx812833 <lx812833@163.com>
2019-01-01 20:13:20 +0800

    router.js

--- src/router.js
+++ src/router.js
@@ -2,16 +2,32 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
+    }, {
+      path: '/register',
+      name: 'Register',
+      component: () => import('./components/pages/Register.vue')
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: () => import('./components/pages/Login.vue')
+    }, {
+      path: '/goods',
+      name: 'Goods',
+      component: () => import('./components/pages/Goods.vue')
+    }, {
+      path: '/categoryList',
+      name: 'CategoryList',
+      component: () => import('./components/pages/CategoryList.vue')
     }
     // {
     //   path: '/about',
