fa57fe882307f48f2f30dd9ab8d3c745b964fc27
lx812833 <lx812833@163.com>
2019-01-02 21:38:57 +0800

    Goods

--- src/components/pages/Goods.vue
+++ src/components/pages/Goods.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
     <div class="navbar-div">
-      <van-nav-bar
-        title="商品详情"
-        left-text="返回"
-        left-arrow
-        @click-left="onClickLeft"
-      />
+      <van-nav-bar title="商品详情" left-text="返回" left-arrow @click-left="onClickLeft" />
     </div>
     <div class="topimage-div">
-      <img
-        :src="goodsInfo.IMAGE1"
-        width="100%"
-      />
+      <img :src="goodsInfo.IMAGE1" width="100%" />
     </div>
     <div class="goods-name">{{ goodsInfo.NAME }}</div>
     <div class="goods-price">价格:${{ goodsInfo.PRESENT_PRICE | moneyFilter }}</div>
@@ -21,10 +13,7 @@
         <!-- swipeable 滑动切换 -->
         <!-- sticky 吸顶效果 -->
         <van-tab title="商品详情">
-          <div
-            class="detail"
-            v-html="goodsInfo.DETAIL"
-          ></div>
+          <div class="detail" v-html="goodsInfo.DETAIL" ></div>
         </van-tab>
         <van-tab title="评论">
           评论
@@ -33,16 +22,10 @@
     </div>
     <div class="goods-bottom">
       <div>
-        <van-button
-          size="large"
-          type="primary"
-        >加入购物车</van-button>
+        <van-button size="large" type="primary" @click="addGoodsToCart">加入购物车</van-button>
       </div>
       <div>
-        <van-button
-          size="large"
-          type="danger"
-        >直接购买</van-button>
+        <van-button size="large" type="danger" >直接购买</van-button>
       </div>
     </div>
   </div>
@@ -60,7 +43,8 @@ export default {
     };
   },
   created() {
-    this.goodsId = this.$route.query.goodsId; // 接收传递过来的goodsId
+    // 接收传递过来的goodsId
+    this.goodsId = this.$route.query.goodsId ? this.$route.query.goodsId : this.$route.params.goodsId;
     console.log(this.goodsId);
     this.getInfo();
   },
@@ -92,6 +76,30 @@ export default {
     },
     onClickLeft() {
       this.$router.go(-1);
+    },
+    // 添加商品到购物车
+    addGoodsToCart() {
+      // 取出本地购物车中的商品并查重
+      let cartInfo = localStorage.cartInfo ? JSON.parse(localStorage.cartInfo) : []
+      let isHaveGoods = cartInfo.find(cart => cart.goodsId == this.goodsId)
+      console.log('isHaveGoods',isHaveGoods)
+      if(!isHaveGoods) {
+        // 购物车中所属的商品信息
+        let newGoods = {
+          goodsId: this.goodsInfo.ID,
+          Name: this.goodsInfo.Name,
+          price:this.goodsInfo.PRESENT_PRICE,
+          image:this.goodsInfo.IMAGE1,
+          count:1
+        }
+        cartInfo.push(newGoods)
+        localStorage.cartInfo = JSON.stringify(cartInfo)  // localStorage本为字符串，要对数据进行处理
+        Toast.success('添加成功')
+      } else {
+        Toast.success('已有此商品')
+      }
+
+      this.$router.push({name:'Cart'})  //进行跳转
     }
   }
 };
